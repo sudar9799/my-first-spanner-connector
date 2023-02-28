cb775b5a27c2049000ab6099df6682994e19c2f5
sudar2bucket_the_bits <sudar.the.professional@gmail.com>
2023-02-28 16:31:54 +0530

    My First spanner connector application

---
+++ my-first-spanner-connector/Controllers/SpannerConnectorController.cs
@@ -0,0 +1,82 @@
+using Google.Cloud.Spanner.Data;
+using Microsoft.AspNetCore.Mvc;
+using my_first_spanner_connector.models;
+
+namespace my_first_spanner_connector.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpannerConnectorController : ControllerBase
+    {
+        string projectId = "";
+        string instanceId = "";
+        string databaseId = "";
+        private readonly ILogger<SpannerConnectorController> _logger;
+
+        public SpannerConnectorController(ILogger<SpannerConnectorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "InsertDummyData")]
+        public async Task Get()
+        {
+            await InsertDataAsync(projectId, instanceId, databaseId);
+        }
+
+        public async Task example()
+        {
+
+            string connectionString =
+                $"Data Source=projects/{projectId}/instances/{instanceId}/"
+                + $"databases/{databaseId}";
+            // Create connection to Cloud Spanner.
+            using (var connection = new SpannerConnection(connectionString))
+            {
+                // Execute a simple SQL statement.
+                var cmd = connection.CreateSelectCommand(
+                    @"SELECT ""Hello World"" as test");
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        Console.WriteLine(
+                            reader.GetFieldValue<string>("test"));
+                    }
+                }
+            }
+        }
+
+        public async Task InsertDataAsync(string projectId, string instanceId, string databaseId)
+        {
+            string connectionString = $"Data Source=projects/{projectId}/instances/{instanceId}/databases/{databaseId}";
+            List<StudentDetails> students = new List<StudentDetails>
+        {
+            new StudentDetails { rollnumber = 5, student_name= "Adhila", department= "Aeronautical", grade="S" },
+            new StudentDetails { rollnumber = 6, student_name= "Yathul", department= "Mechanical", grade="A" }
+        };
+
+            // Create connection to Cloud Spanner.
+            using var connection = new SpannerConnection(connectionString);
+            await connection.OpenAsync();
+
+            await connection.RunWithRetriableTransactionAsync(async transaction =>
+            {
+                await Task.WhenAll(students.Select(student =>
+                {
+                    // Insert rows into the Singers table.
+                    using var cmd = connection.CreateInsertCommand("student_details", new SpannerParameterCollection
+                {
+                        { "rollnumber", SpannerDbType.Int64, student.rollnumber },
+                        { "student_name", SpannerDbType.String, student.student_name },
+                        { "department", SpannerDbType.String, student.department },
+                        { "grade", SpannerDbType.String, student.grade }
+                });
+                    cmd.Transaction = transaction;
+                    return cmd.ExecuteNonQueryAsync();
+                }));
+            });
+            Console.WriteLine("Data inserted.");
+        }
+    }
+}
---
+++ my-first-spanner-connector/models/StudentDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace my_first_spanner_connector.models
+{
+    public class StudentDetails
+    {
+        public int rollnumber { get; set; }
+        public string student_name { get; set; }
+        public string department { get; set; }
+        public string grade { get; set; }
+    }
+}
